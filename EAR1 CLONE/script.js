cae24d6b9e95b645904b96ac4a023269744f508b
Chidimma Uchenabo <92271356+Dimmmamma@users.noreply.github.com>
2022-08-10 19:33:19 +0100

    Uploaded other files including images, etc

---
+++ EAR1 CLONE/script.js
@@ -0,0 +1,17 @@
+// const phone1 = document.querySelector(".phone1")
+// const phone2 = document.querySelector(".phone2")
+// const phone3 = document.querySelector(".phone3")
+// const phones = document.querySelector(".phones")
+
+
+// function clickHandler(event) {
+//   console.log('Img Clicked');
+// }
+// const menu = document.querySelectorAll('.menu');
+// menu.addEventListener('click', clickHandler);
+
+// const controller = new
+// AbortController ();
+// const el =
+// document.getElementById("menuimg");
+// el.addEventListener("click")
